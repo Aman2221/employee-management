2525e19fd117e444fc6fc431f007be4d2bf06cdd
Aman2221 <as562770@gmail.com>
2024-09-14 19:15:23 +0400

    dark and light mode css updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-white dark:bg-gray-900 fixed w-full z-20 bottom-0 start-0 border-t  border-gray-200 dark:border-gray-600 py-6">
+    <div className="bg-white dark:bg-gray-900 fixed w-full z-20 bottom-0 start-0 border-t   border-gray-200 dark:border-gray-600 py-6">
       <h1 className="text-center">
         Copyright © 2024{" "}
         <span className="uppercase font-bold text-center">primasoft est</span>.
