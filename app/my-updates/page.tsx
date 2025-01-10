61f65ff8bb84605ba04a2f56033eff1bb1913ff4
Aman2221 <as562770@gmail.com>
2025-01-10 22:58:18 +0400

    Added react suspense for useSearchParams

--- app/my-updates/page.tsx
+++ app/my-updates/page.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/Common/Layout";
 import MyUpdatesPg from "@/components/Updates/MyUpdatesPg";
-import React from "react";
+import React, { Suspense } from "react";
 
 const MyUpdates = () => {
-  return <Layout>{<MyUpdatesPg />}</Layout>;
+  return (
+    <Suspense>
+      <Layout>{<MyUpdatesPg />}</Layout>
+    </Suspense>
+  );
 };
 
 export default MyUpdates;
