844283dccc127ffe81e14493b5cb9f4cf06426f0
Aman2221 <as562770@gmail.com>
2025-02-10 11:01:33 +0400

    added view leaves in Suspense

--- app/view-leaves/page.tsx
+++ app/view-leaves/page.tsx
@@ -1,9 +1,13 @@
 import HomeComp from "@/components/HomeComp";
 import Layout from "@/components/Common/Layout";
-import React from "react";
+import React, { Suspense } from "react";
 
 const HomePg = () => {
-  return <Layout>{<HomeComp />}</Layout>;
+  return (
+    <Suspense>
+      <Layout>{<HomeComp />}</Layout>
+    </Suspense>
+  );
 };
 
 export default HomePg;
