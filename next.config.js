15b4bf74967678c4658849e2ffeaa858d1e5de34
Aman2221 <as562770@gmail.com>
2025-03-25 12:43:57 +0400

    github action integration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 // next.config.js
 
+// const withPWA = require("next-pwa")({
+//   dest: "public", // where the service worker is generated
+//   disable: process.env.NEXT_PUBLIC_DEV_ENVIRONMENT === "development", // disable PWA in development mode
+// });
+
+// module.exports = withPWA({
+//   // Additional Next.js config options here
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+// });
+
+/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-  dest: "public", // where the service worker is generated
-  disable: process.env.NEXT_PUBLIC_DEV_ENVIRONMENT === "development", // disable PWA in development mode
+  dest: "public",
+  disable: process.env.NEXT_PUBLIC_DEV_ENVIRONMENT === "development",
 });
 
 module.exports = withPWA({
