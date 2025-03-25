5a60da74909eac28deb0b7778c4794119be5d21e
Aman2221 <as562770@gmail.com>
2025-03-25 17:09:20 +0400

    github actions secrets

--- app/api/delete-user/route.ts
+++ app/api/delete-user/route.ts
@@ -10,10 +10,6 @@ if (!admin.apps.length) {
             private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
             client_email: process.env.FIREBASE_CLIENT_EMAIL,
         };
-        console.log("serviceAccount", serviceAccount);
-        console.log('Private Key EXISTS:', !!process.env.FIREBASE_PRIVATE_KEY)
-        console.log('Client Email EXISTS:', !!process.env.FIREBASE_CLIENT_EMAIL)
-        console.log('Project ID EXISTS:', !!process.env.FIREBASE_PROJECT_ID)
 
         if (!serviceAccount.private_key || !serviceAccount.client_email) {
             throw new Error(
