de81c1c01c295c378bf5219eb7ddaec96972ce44
Aman2221 <as562770@gmail.com>
2025-03-25 14:18:37 +0400

    added updated enviromental variable

--- next.config.js
+++ next.config.js
@@ -23,4 +23,9 @@ module.exports = withPWA({
   images: {
     domains: ["res.cloudinary.com"],
   },
+  env: {
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  },
 });
